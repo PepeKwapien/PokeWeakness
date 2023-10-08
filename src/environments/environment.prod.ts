19766a2c8b83b5f4b047f9557e6e81596f001c6e
PepeKwapien <pppkwapien@gmail.com>
2023-10-08 22:54:39 +0200

    Added my azure url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,1 @@
-export const environment = { pokemonApiUrl: 'https://localhost:7237', env: 'production' };
+export const environment = { pokemonApiUrl: 'https://pokemonapipepe.azurewebsites.net', env: 'production' };
